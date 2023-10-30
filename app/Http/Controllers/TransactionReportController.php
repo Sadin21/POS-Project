8594780f3847e82711b8ffcdec34b2006a46f0d6
Daffa Asaddin <emdeasaaddin21@gmail.com>
2023-10-30 20:51:18 +0700

    update chart view

--- app/Http/Controllers/TransactionReportController.php
+++ app/Http/Controllers/TransactionReportController.php
@@ -13,28 +13,22 @@ use Illuminate\Support\Facades\DB;
 
 class TransactionReportController extends Controller
 {
-    // public function index(): View {
-    //     $chart_options = [
-    //         'chart_title' => 'Products by months',
-    //         'report_type' => 'group_by_date',
-    //         'model' => 'App\Models\Product',
-    //         'group_by_field' => 'created_at',
-    //         'group_by_period' => 'month',
-    //         'chart_type' => 'bar',
-    //     ];
-    //     $chart1 = new LaravelChart($chart_options);
-
-    //     return view('pages.master.transaction.index', compact('chart1'));
-    // }
-
     public function index(): View {
-        $users = User::select(DB::raw("COUNT(*) as count"))
-            ->whereYear ('created_at', date('Y'))
-            ->groupBy(DB::raw("Month(created_at)"))
-            ->pluck('count');
-
-        $labels = $users->keys();
-        $data = $users->values();
+        $sales = SaleInvoiceHdr::select(
+            DB::raw("COUNT(*) as count"),
+            DB::raw("DATE(created_at) as date")
+        )
+        ->whereYear('created_at', date('Y'))
+        ->groupBy(DB::raw("DATE(created_at)"))
+        ->get();
+        
+        $labels = [];
+        $data = [];
+        
+        foreach ($sales as $sale) {
+            $labels[] = $sale->date; 
+            $data[] = $sale->count; 
+        }
 
         return view('pages.master.report.index', compact('labels', 'data'));
     }
--- resources/views/pages/master/report/index.blade.php
+++ resources/views/pages/master/report/index.blade.php
@@ -6,10 +6,6 @@
     <div class="d-flex justify-content-between align-items-center flex-shrink-0 gap-4">
         <h4 class="m-0">Laporan Penjualan</h4>
 
-        {{-- <a class="btn btn-primary d-flex align-items-center gap-2 btn-sm" href="">
-            <ion-icon name="add" class="f24"></ion-icon>
-            Tambah Kategori
-        </a> --}}
     </div>
     <div class="card border-0 flex-grow-1 d-flex flex-column h-100">
         <div class="border-bottom px-4 pt-4 pb-3 flex-shrink-0">
@@ -24,15 +20,15 @@
 <script type="text/javascript">
   
     var labels =  {{ Js::from($labels) }};
-    var users =  {{ Js::from($data) }};
+    var datas =  {{ Js::from($data) }};
 
     const data = {
       labels: labels,
       datasets: [{
         label: 'Data Penjualan',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: users,
+        backgroundColor: 'rgb(0, 0, 255)',
+        borderColor: 'rgb(0, 0, 255)',
+        data: datas,
       }]
     };
 
@@ -49,36 +45,4 @@
 
 </script>
 
-{{-- @section('script')
-import Chart from 'chart.js/auto';
-
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-];
-
-const data = {
-    labels: labels,
-    datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-};
-
-const config = {
-    type: 'line',
-    data: data,
-    options: {}
-};
-
-new Chart(
-    document.getElementById('myChart'),
-    config
-); --}}
 @endsection
